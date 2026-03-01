0b287e63cd9255bb4fe70ce43902630d778b2c27
Mark Lights <marklights899@gmail.com>
2026-03-01 04:04:16 -0500

    updates

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,7 +1,8 @@
 import React from 'react'
-
 import '../css/portfolio.css'
 import lightsFinaceImg from '../files/Lights_Finance.png'
+import rag_ui from '../files/Rag UI.png'
+import compressPDF_UI from '../files/compreePDF UI.png'
 const Portfolio = () => {
   return (
     <div className='portfolio-body'>
@@ -34,7 +35,75 @@ const Portfolio = () => {
                 <img className='lightsFinanceLandingPage' src={lightsFinaceImg} alt='landing page of website' onClick={() => {window.location.href = 'https://marks-pi.com/finance'}}/>
             </div>
         </div>
-        
+
+        <div className='projects-list'>
+            <div className='left-side-of-project'>
+                <h2>Rag Pipeline</h2>
+                <h3>Description:</h3>
+                <ul>
+                    <li>
+                        Developed a document retrieval and question-answering application that allows
+                        users to upload PDF and TXT files and query their contents using semantic
+                        search. Implemented a Python/FastAPI backend utilizing the all-MiniLM-L6-v2
+                        embedding model and ChromaDB for vector storage and similarity search. Built a
+                        React/Express frontend for user interaction and deployed the system on a
+                        self-hosted Raspberry Pi 5 environment.
+                    </li>
+                </ul>
+                <h3>Skills/Technologies used:</h3>
+                <ul>
+                    <li>FastAPI</li>
+                    <li>ChromaDB</li>
+                    <li>Docker</li>
+                    <li>Rag (Retrieval-Augmented Generation)</li>
+                    <li>Linux</li>
+                    <li>Bash</li>
+                    <li>Hosting:</li>
+                    <ul>
+                        <li>Raspberry Pi</li>
+                        <li>Cloudflare tunneling</li>
+                    </ul>
+                </ul>
+            </div>
+
+            <div className='right-side-of-project'>
+                <img className='lightsFinanceLandingPage' src={rag_ui} alt='landing page of website' onClick={() => {window.location.href = 'https://marks-pi.com/rag/'}}/>
+            </div>
+        </div>      
+
+        <div className='projects-list'>
+            <div className='left-side-of-project'>
+                <h2>Compress PDF</h2>
+                <h3>Description:</h3>
+                <ul>
+                    <li>
+                        For anybody who gets tired of searching for a pdf compressor and you upload
+                        your files. Just to find out it requires your email to download it. Or worse you
+                        have a set limit on how many you can upload and it charges you a fee. This is for
+                        you. It's simple, free, and unlimited. All files uploaded are instantly
+                        deleted after being compressed and returned back to you. There is a catch only 1
+                        file at a time.
+                    </li>
+                </ul>
+                <h3>Skills/Technologies used:</h3>
+                <ul>
+                    <li>FastAPI</li>
+                    <li>React.js</li>
+                    <li>Linux</li>
+                    <li>Bash</li>
+                    <li>python</li>
+                    <li>Hosting:</li>
+                    <ul>
+                        <li>Raspberry Pi</li>
+                        <li>Cloudflare tunneling</li>
+                    </ul>
+                </ul>
+            </div>
+
+            <div className='right-side-of-project'>
+                <img className='lightsFinanceLandingPage' src={compressPDF_UI} alt='landing page of website' onClick={() => {window.location.href = 'https://marks-pi.com/compressPDFApp/'}}/>
+            </div>
+        </div>    
     </div>
   )
 }
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,31 +1,55 @@
 import React, {useEffect, useState} from 'react'
 import '../css/resume.css'
-import resume from '../files/Mark_Lights_Resume_Aug_24.png'
-import resume1 from '../files/Resume Dec_25-1.png'
-import resume2 from '../files/Resume Dec_25-2.png'
+// import resume from '../files/Mark_Lights_Resume_Aug_24.png'
+// import resume1 from '../files/Resume Dec_25-1.png'
+// import resume2 from '../files/Resume Dec_25-2.png'
+import resume from '../files/Lights_Mark_Resume_March_1_2026.pdf'
 const Resume = () => {
   const [mediumDevice,setMediumDevice] = useState(false)
   
   useEffect(() =>{
     window.addEventListener('resize', () =>{
       if(window.innerWidth < 499){
-        setMediumDevice(!mediumDevice)
+        setMediumDevice(true)
       }else{
-        setMediumDevice(!mediumDevice)
+        setMediumDevice(false)
       }
     })
 
     if(window.width < 499){
-      setMediumDevice(!mediumDevice)
+      setMediumDevice(true)
     }else{
-      setMediumDevice(!mediumDevice)
+      setMediumDevice(false)
     }
   },[])
 
   return (
     <>
-      <img className='resume1' src={resume1} alt='lightsFinance'/>
-      <img className='resume1' src={resume2} alt='lightsFinance'/>
+      {/* <img className='resume1' src={resume1} alt='lightsFinance'/>
+      <img className='resume1' src={resume2} alt='lightsFinance'/> */}
+      {mediumDevice ?
+      <div style={{textAlign:'center'}}>
+        <a
+          href={resume}
+          rel="noopener noreferrer"
+          style={{ display: "block", marginBottom: "8px", textDecoration: "none", color: 'white' }}
+          >
+            RESUME
+        </a>
+      </div>
+      
+
+      :
+
+      <div style={{height: '85vh', textAlign: 'center'}}>
+        <iframe
+          src={resume}
+          style={{ border: "none", minHeight:"100%", minWidth: "90%" }}
+        />
+      </div>
+      }
+      
+      
     </>
     
   )
